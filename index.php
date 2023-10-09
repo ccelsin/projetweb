68a6847cd247b29034ec8ac4018e267ecf96b6c8
ccelsin <124219444+ccelsin@users.noreply.github.com>
2023-10-09 11:18:58 +0200

    picture

--- index.php
+++ index.php
@@ -4,15 +4,6 @@
 ?>
 
 <body>
-   
-    <form action="index_traitement.php" method="POST">
-        votre nom:<input type="text" name="nom" ><br>
-        votre prenom:<input type="text" name="prenom" ><br>
-        votre age:<input type="number" name="age" ><br>
-         password:<input type="password" name="password" ><br>
-        <input type="submit" value="Envoyer !" ><br>
-
-
-    </form>
+    <div></div>
 </body>
 </html>
