90184c5b1d696c4548a491a927c6a7a799a8cfc6
Celsin ADJAGBONI <celsinadjagboni@gmail.com>
2023-10-18 19:51:15 +0200

    commit

--- index.php
+++ index.php
@@ -5,21 +5,18 @@
 
 <body>
     <main>
-      <p>Bienvenue à la <span class="text-primary">Family League Arena!</span>  </p>
+      <p>Bienvenue à la <span class="text-primary"> Family League Arena! </span>  </p>
       <p>Family League Arena!</p>
-
       <p>Rejoignez le plus grand jeux de table</p>
       <p>au monde</p>
       <p>Qu'attendez vous!!</p>
       <form action="index_traitement.php" method="POST" >
         <input type="submit" value="Commencez à jouer maintenant !" ><br>
-
-
-
-    </form>
+      </form>
     </main>
 
     
 </body>
+
 </html>
 
