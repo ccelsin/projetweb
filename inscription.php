3cde22bd4497b3d6cab867f1f3b41bb9b526fc73
Celsin ADJAGBONI <celsinadjagboni@gmail.com>
2023-10-18 15:12:14 +0200

    akee

--- inscription.php
+++ inscription.php
@@ -3,19 +3,18 @@
     include("header.inc.php");
 ?>
 <body>
-<?php
-
 <img src="/picture/fond d'écran app.png" alt="fond d'ecran">
+<?php
     echo "<p>Bienvenue a la Family League Arena!</p>";
     echo "<p>Rejoignez le plus grand jeux de table</p>";
     echo "<p>au monde</p>";
     echo "<p>Qu'attendez vous!!</p>";
-
-   
     echo "<p>Bonjour en php!!</p>";
-    $maintenant = new DateTime();
+   
+    /*$maintenant = new DateTime();
     $maintenant->setTimezone(new DateTimezone('Europe/Paris'));
-    echo"<p>Nous sommes le".$maintenant-> format('d/m/Y')."</p>\n";
+    echo"<p>Nous sommes le".$maintenant-> format('d/m/Y')."</p>\n";*/
+    
     ?>
 
 </body>
