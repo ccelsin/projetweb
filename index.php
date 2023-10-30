8075a0d6f5f032b387ca7d9127303be3e6180fc7
Emmanuel AGBAHOLOU <manuagbaholou@gmail.com>
2023-10-30 02:26:22 +0100

    iejd

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
             <p class="fs-3"><span class="text-primary">Rejoignez</span> le plus grand jeux de table</p>
             <p class="text-primary fs-3">au monde</p>
             <p class="text-primary fs-3">Qu'attendez vous!!</p>
+            
               <div class="container">
                   <div class="row"  style="margin-top :80vh">
                     <div class="col-4"></div>
@@ -21,16 +22,17 @@
                       </div>
                 </div>
             </div>
+            
+
             <div class="row">
               <p>
-                <br><br><br><br><br>
+                <br><br><br>
               </p>
             </div>
             <div class="row ">
               <div class="col-lg-4">
               <img src="images/ordi.png" class="rounded float-start img-fluid " id="monitor">
               </div>
-
               <div class="col-lg-3">
               <p>
                 <br><br><br><br><br>
