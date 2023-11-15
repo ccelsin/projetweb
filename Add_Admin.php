e3eed80f15ced5cadfc73d595702354f9770b385
Emmanuel AGBAHOLOU <manuagbaholou@gmail.com>
2023-11-16 00:26:23 +0100

    Nouvelles images

---
+++ Add_Admin.php
@@ -0,0 +1,48 @@
+<?php
+    $title_page = " page d'ajout de d'admin'";
+    include("header.inc.php");
+?>
+<main>
+ 
+  <div class="d-grid gap-2 col-6 mx-auto">
+    <form action="espace_Admin.php" method="POST" >
+  <div>
+    
+    <div class="mb-3 row">
+    <label for="nom" class="col-sm-2 col-form-label">nom</label>
+    <div class="col-sm-10">
+      <input type="nom"  class="form-control" nom="nom" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="prenom" class="col-sm-2 col-form-label">prenom</label>
+    <div class="col-sm-10">
+      <input type="prenom" class="form-control" prenom="prenom" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="email" class="col-sm-2 col-form-label">email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" email="email" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="Password" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="Password" class="form-control" Password ="Password" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="numéro" class="col-sm-2 col-form-label">Contact</label>
+    <div class="col-sm-10">
+      <input type="numéro" class="form-control" Contact ="numéro" required>
+    </div>
+  </div>
+     
+  </div>
+  <button type="Suivant" class="btn btn-outline-primary btn-lg">Suivant</button>
+  </form>
+  
+    </div>
+  
+</main>
