6be0f7e134bf7ba36fa3bfca9f9291f4f8763bbc
Celsin ADJAGBONI <celsinadjagboni@gmail.com>
2023-11-16 02:23:23 +0100

    Modification du code et authentification

--- connexion.php
+++ connexion.php
@@ -1,18 +1,19 @@
 <?php
-    $title_page = " page de connexion";
+
+   $title_page = " page de connexion";
     include("header.inc.php");
 ?>
 
   <form class="form" action="espace_Admin.php" method="POST" style="margin:50px;">
     <div class="form-group">
       <label for="email" class="form-label">Email address</label>
-      <input type="text" id="email" class="form-control" placeholder="name@example.com" required>
+      <input type="text" name="email" id="email" class="form-control" placeholder="name@example.com" required>
     </div>
     <div class="form-group">
       <label for="password" class="form-label">Password</label>
-      <input type="password" id="password" class="form-control" placeholder="*******" required>
+      <input type="password" name="password" id="password" class="form-control" placeholder="*******" required>
     </div>
     
-    <button type="Se connecter" class="btn btn-outline-primary btn-lg">Se connecter</button>
+    <button type="submit" class="btn btn-outline-primary btn-lg">Se connecter</button>
   </form>
   
