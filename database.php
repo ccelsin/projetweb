8f030c7c96a37d113989dc3cb640191ce0ff9ea9
Celsin ADJAGBONI <celsinadjagboni@gmail.com>
2023-11-18 01:45:25 +0100

    Ajout d'un user à la base de données/ Inscription

--- database.php
+++ database.php
@@ -28,6 +28,4 @@
 
     }
 
-    Database::connect();
-
 ?>
--- inscription.php
+++ inscription.php
@@ -2,12 +2,26 @@
     //On vérifie si les champs existent et s'ils ont été remplis
     if(isset($_POST["nom"],$_POST["prenom"]) && !empty($_POST["nom"]) && !empty($_POST["prenom"])){
 
+      $firstname = strip_tags($_POST["nom"]);
+      $lastname = strip_tags($_POST["prenom"]);
+
+      //On enregistre les premières informations dans la base de données
+
+      require_once"database.php";
+      $db = Database::connect();
+      $sql = "INSERT INTO `utilisateurs` (`id`, `first_name`, `last_name`, `email`, `password`, `avatar`, `privilège`) VALUES (NULL, ':firstname', ':lastname', '', '', '', 'membre')";
+      $statement = $db->prepare($sql);
+      $statement->bindValue(":firstname", $firstname, PDO::PARAM_STR);
+      $statement->bindValue(":lastname", $lastname, PDO::PARAM_STR);
+      $statement->execute();
+
     }else{
-      die("Le formulaire est imcomplet");
+      //die("Le formulaire est imcomplet");
     }
 
 
     $title_page = " page de renseignement";
+
     include("header.inc.php");
 ?>
  
@@ -17,35 +31,18 @@
         <div class="mb-3 row">
           <label for="nom" class="col-sm-2 col-form-label">nom</label>
           <div class="col-sm-10">
-             <input type="nom" name="nom" class="form-control" nom="nom" required>
+             <input type="text" name="nom" class="form-control" required>
           </div>
         </div>
         <div class="mb-3 row">
           <label for="prenom" class="col-sm-2 col-form-label">prenom</label>
           <div class="col-sm-10">
-            <input type="prenom" name="prenom" class="form-control" prenom="prenom" required>
+            <input type="text" name="prenom" class="form-control" required>
           </div>
         </div>
-        <div class="mb-3 row">
-          <label for="adresse" class="col-sm-2 col-form-label">adresse</label>
-          <div class="col-sm-10">
-            <input type="adresse"  class="form-control" adresse="adresse" required>
-          </div>
-        </div>
-        <div class="mb-3 row">
-          <label for="code_postale" class="col-sm-2 col-form-label">Code Postale</label>
-          <div class="col-sm-10">
-            <input type="code_postale" class="form-control" Code Postale ="code_postale" required>
-          </div>
-        </div>
-        <div class="mb-3 row">
-          <label for="numéro" class="col-sm-2 col-form-label">Contact</label>
-          <div class="col-sm-10">
-            <input type="numéro" class="form-control" Contact ="numéro" required>
-          </div>
-        </div>
+       
       </div>
-      <button type="Suivant" class="btn btn-outline-primary btn-lg">Suivant</button>
+      <button type="submit" class="btn btn-outline-primary btn-lg">Suivant</button>
     </form>
   </div>
 
