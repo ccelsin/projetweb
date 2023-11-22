b9c802cb937307f7decef0b8e1baf837fcb11962
Emmanuel AGBAHOLOU <manuagbaholou@gmail.com>
2023-11-22 16:54:47 +0100

    tt CONNEXION ET INSCRIPTION

--- tt_connexion.php
+++ tt_connexion.php
@@ -47,24 +47,148 @@ if ($stmt = $con->prepare("SELECT * FROM utilisateurs WHERE email=? LIMIT 1"))
             // Redirection en fonction du rôle
             if ($row["statut"] == 1)
             {
-                $_SESSION['message'] = "Authentification réussie pour un admin.";
-                header('Location: espace_Admin.php');
+                echo ' <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+   
+   
+      <script src="../js/bootstrap.min.js"></script>';
+   
+        echo '<div id="bienvenue-toast" class="toast position-fixed top-50 start-50 translate-middle" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+          <strong class="me-auto">Notification</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+          connexion réussie!
+        </div>
+      </div>';
+   
+        echo ' <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>';
+   
+   
+        echo "<script>
+        // Affiche le toast de bienvenue après 1 seconde
+   
+        setTimeout(function () {
+          var bienvenueToast = new bootstrap.Toast(document.getElementById('bienvenue-toast'));
+          bienvenueToast.show();
+          toast.hide();
+   
+        }, 200);
+     
+        setTimeout(function () {
+            window.location.href = 'espace_Admin.php';
+          }, 4000);
+      </script>";
             } elseif ($row["statut"] == 0)
                {
-                $_SESSION['message'] = "Authentification réussie pour un utilisateur.";
-                header('Location: espace_perso.php');
+                echo ' <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+           
+           
+              <script src="../js/bootstrap.min.js"></script>';
+           
+                echo '<div id="bienvenue-toast" class="toast position-fixed top-50 start-50 translate-middle" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="toast-header">
+                  <strong class="me-auto">Notification</strong>
+                  <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                  connexion réussie!
+                </div>
+              </div>';
+           
+                echo ' <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>';
+           
+           
+                echo "<script>
+                // Affiche le toast de bienvenue après 1 seconde
+           
+                setTimeout(function () {
+                  var bienvenueToast = new bootstrap.Toast(document.getElementById('bienvenue-toast'));
+                  bienvenueToast.show();
+                  toast.hide();
+           
+                }, 200);
+             
+                setTimeout(function () {
+                    window.location.href = 'espace_perso.php';
+                  }, 4000);
+              </script>";
                }
 
         } else {
             // Redirection si le mot de passe est incorrect
-            $_SESSION['message'] = "Mot de passe ou email incorrect.";
-            header('Location: connexion.php');
+            echo ' <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+   
+   
+      <script src="../js/bootstrap.min.js"></script>';
+   
+        echo '<div id="bienvenue-toast" class="toast position-fixed top-50 start-50 translate-middle" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+          <strong class="me-auto">Notification</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+          Identifiant ou mot de passe incorrect!
+        </div>
+      </div>';
+   
+        echo ' <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>';
+   
+   
+        echo "<script>
+        // Affiche le toast de bienvenue après 1 seconde
+   
+        setTimeout(function () {
+          var bienvenueToast = new bootstrap.Toast(document.getElementById('bienvenue-toast'));
+          bienvenueToast.show();
+          toast.hide();
+   
+        }, 200);
+     
+        setTimeout(function () {
+            window.location.href = 'connexion.php';
+          }, 4000);
+      </script>";
         }
     }
      else {
         // Redirection si l'utilisateur n'existe pas
-        $_SESSION['message'] = "Identifiant inexistant.";
-        header('Location: index.php');
+        echo ' <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+   
+   
+      <script src="../js/bootstrap.min.js"></script>';
+   
+        echo '<div id="bienvenue-toast" class="toast position-fixed top-50 start-50 translate-middle" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+          <strong class="me-auto">Notification</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+          Utilisateur inexistant!
+        </div>
+      </div>';
+   
+        echo ' <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>';
+   
+   
+        echo "<script>
+        // Affiche le toast de bienvenue après 1 seconde
+   
+        setTimeout(function () {
+          var bienvenueToast = new bootstrap.Toast(document.getElementById('bienvenue-toast'));
+          bienvenueToast.show();
+          toast.hide();
+   
+        }, 200);
+     
+        setTimeout(function () {
+            window.location.href = 'index.php';
+          }, 4000);
+      </script>";
     }
 
  
@@ -78,3 +202,18 @@ if ($stmt = $con->prepare("SELECT * FROM utilisateurs WHERE email=? LIMIT 1"))
 // Fermer la connexion à la base de données
 $con->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</body>
+</html>
