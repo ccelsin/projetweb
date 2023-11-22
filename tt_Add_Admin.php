a048e153aada08a9225b1007de147c9da1af4bd6
Emmanuel AGBAHOLOU <manuagbaholou@gmail.com>
2023-11-22 16:58:17 +0100

    traitement add admin

--- tt_Add_Admin.php
+++ tt_Add_Admin.php
@@ -1,33 +1,73 @@
+
 <?php
-  session_start(); // Pour les messages
+session_start(); // Pour les messages
 
-  // Contenu du formulaire :
-  $nom =  htmlentities($_POST['nom']);
-  $prenom = htmlentities($_POST['prenom']);
-  $email =  htmlentities($_POST['email']);
-  $password = htmlentities($_POST['password']);
-  $role = 1; 
+// Contenu du formulaire :
+$nom = htmlentities($_POST['nom']);
+$prenom = htmlentities($_POST['prenom']);
+$email = htmlentities($_POST['email']);
+$password = htmlentities($_POST['password']);
+$statut = 1;
 
-  // Option pour bcrypt
-  $options = [
-        'cost' => 12,
-  ];
+// Option pour bcrypt
+$options = [
+    'cost' => 12,
+];
 
-  // Connexion 
-  require_once("database1.php");
-  $con = new mysqli($host, $login, $passwd, $dbname);
-  if ($con->connect_error) {
-      die('Erreur de connexion (' . $con->connect_errno . ') '
-              . $con->connect_error);
-  }
-else{
-// Attention, ici on ne vérifie pas si l'utilisateur existe déjà
-if ($stmt = $con->query("INSERT INTO utilisateurs(nom, prenom, email, mdp, statut) VALUES ('$nom','$prenom', '$email', '$password', '$role')")) {
-    //$password = password_hash($password, PASSWORD_BCRYPT, $options);
-    // Le message est mis dans la session, il est préférable de séparer message normal et message d'erreur.
-    if($stmt) {
-       
-       
+// Connexion :
+require_once("database1.php");
+$con = new mysqli($host, $login, $passwd, $dbname);
+if ($con->connect_error) {
+    die('Erreur de connexion (' . $con->connect_errno . ') '
+            . $con->connect_error);
+} else {
+    // Vérifier si l'utilisateur existe déjà
+    $checkUserQuery = "SELECT * FROM utilisateurs WHERE email = '$email'";
+    $result = $con->query($checkUserQuery);
+
+    if ($result->num_rows > 0) {
+        // L'utilisateur existe déjà, afficher un message d'erreur
+        echo ' <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+ 
+ 
+    <script src="../js/bootstrap.min.js"></script>';
+ 
+      echo '<div id="bienvenue-toast" class="toast position-fixed top-50 start-50 translate-middle" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <strong class="me-auto">Notification</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body">
+      Admin existant  déjà.!
+      </div>
+    </div>';
+ 
+      echo ' <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>';
+ 
+ 
+      echo "<script>
+      // Affiche le toast de bienvenue après 1 seconde
+ 
+      setTimeout(function () {
+        var bienvenueToast = new bootstrap.Toast(document.getElementById('bienvenue-toast'));
+        bienvenueToast.show();
+        toast.hide();
+ 
+      }, 200);
+   
+      setTimeout(function () {
+          window.location.href = 'index.php';
+        }, 4000);
+    </script>";
+    }  else {
+      // Hacher le mot de passe
+      $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+      // Insérer l'utilisateur avec le mot de passe haché
+      $insertUserQuery = "INSERT INTO utilisateurs (nom,prenom,email,mdp,statut) values ('$nom','$prenom','$email','$hashedPassword','$statut')";
+
+      if ($con->query($insertUserQuery) === TRUE) {
         echo ' <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
    
@@ -40,7 +80,7 @@ if ($stmt = $con->query("INSERT INTO utilisateurs(nom, prenom, email, mdp, statu
           <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
-          Administrateur créée avec succès
+          Génial!! Un administrateur vient est créé!
         </div>
       </div>';
    
@@ -58,55 +98,19 @@ if ($stmt = $con->query("INSERT INTO utilisateurs(nom, prenom, email, mdp, statu
         }, 200);
      
         setTimeout(function () {
-            window.location.href = 'espace_Admin.php';
+            window.location.href = 'connexion.php';
           }, 4000);
       </script>";
-
-
-    } else {
-        
-        echo ' <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-   
-   
-      <script src="../js/bootstrap.min.js"></script>';
-   
-        echo '<div id="bienvenue-toast" class="toast position-fixed top-50 start-50 translate-middle" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-          <strong class="me-auto">Notification</strong>
-          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-          Cet utilisateur existe deja!
-        </div>
-      </div>';
-   
-        echo ' <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>';
-   
-   
-        echo "<script>
-        // Affiche le toast de bienvenue après 1 seconde
-   
-        setTimeout(function () {
-          var bienvenueToast = new bootstrap.Toast(document.getElementById('bienvenue-toast'));
-          bienvenueToast.show();
-          toast.hide();
-   
-        }, 200);
-     
-        setTimeout(function () {
-            window.location.href = 'index.php';
-          }, 4000);
-      </script>";
-
-    }
+      } else {
+          echo "Erreur lors de l'inscription : " . $con->error;
+      }
   }
-  
 }
-  
 
+$con->close();
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
