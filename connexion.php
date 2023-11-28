0afa84805d898ed95b8536bf3e0135a15349d894
Farha15 <farhanathladany@gmail.com>
2023-11-28 22:29:05 +0100

    nav inscription connexion

--- connexion.php
+++ connexion.php
@@ -4,6 +4,40 @@
     include'header.inc.php';
     include 'database1.php';
 ?>
+
+
+
+
+<nav class="navbar navbar-expand-md bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="index.php">Family League Arena</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-lg-0">
+      <a class="navbar-brand" href="Liste_Jeux.php">Nos Jeux</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        </ul>
+        <ul class="navbar-nav mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="inscription.php">S'inscrire</a>
+        </li>
+        
+</ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
 <div class="d-grid d-flex justify-content-center align-items-center gap-3 col-8 mx-auto col-lg-4 col-sm-8" style="margin-top:25vh">
   <form  action="tt_connexion.php" method="POST" >
     <div class="form-group">
--- inscription.php
+++ inscription.php
@@ -4,6 +4,38 @@ session_start();
     include("header.inc.php");
 ?>
 
+
+<nav class="navbar navbar-expand-md bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="index.php">Family League Arena</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-lg-0">
+      <a class="navbar-brand" href="Liste_Jeux.php">Nos Jeux</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        </ul>
+        <ul class="navbar-nav mb-lg-0">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="connexion.php">Se connecter</a>
+        </li>
+</ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
 <div class="d-grid d-flex justify-content-center align-items-center gap-3 col-8 mx-auto col-lg-4 col-sm-8" style="margin-top:18vh">
     <form action="tt_inscription.php" method="POST" >
         <div class="form-group">
