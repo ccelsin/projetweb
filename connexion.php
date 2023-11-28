aebf3e299a765f67eca7bd19720d93b919b7e1d9
Farha15 <farhanathladany@gmail.com>
2023-11-28 22:12:10 +0100

    modif connexion

--- connexion.php
+++ connexion.php
@@ -1,21 +1,21 @@
 <?php
-session_start();
+    session_start();
     $title_page = " page de connexion";
     include'header.inc.php';
     include 'database1.php';
 ?>
-
-
-  <form class="form" action="tt_connexion.php" method="POST" style="margin:50px;">
+<div class="d-grid d-flex justify-content-center align-items-center gap-3 col-8 mx-auto col-lg-4 col-sm-8" style="margin-top:25vh">
+  <form  action="tt_connexion.php" method="POST" >
     <div class="form-group">
-      <label for="email" class="form-label">Email address</label>
+      <label for="email" class="col-sm-2 col-form-label">Email address</label>
       <input type="text" name="email" id="email" class="form-control" placeholder="name@example.com" required>
     </div>
     <div class="form-group">
-      <label for="password" class="form-label">Password</label>
+      <label for="password" class="col-sm-2 col-form-label">Password</label>
       <input type="password" name="password"  class="form-control" placeholder="*******" required>
     </div>
     
   <button type="submit" class="btn btn-outline-primary btn-lg">Se connecter</button>
   </form>
+  </div>
   
